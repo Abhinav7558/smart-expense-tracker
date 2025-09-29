bb32295d82d169f825c3fffaa3a901572bec79c6
Abhinav <abhinavdas2301@gmail.com>
2025-09-29 17:20:04 +0530

    feat: Added sample.env and readme filr

--- sme_backend/app/database.py
+++ sme_backend/app/database.py
@@ -1,8 +1,12 @@
+import os
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-DATABASE_URL = "sqlite:///./expense_tracker.db"
+load_dotenv()
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 metadata = MetaData()
